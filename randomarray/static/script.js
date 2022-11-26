8fa93328a882f04cc6cfde98f6ce8350146f4959
DDonghyeo <gsu9045000@gmail.com>
2022-11-27 01:54:22 +0900

    duplicate check

--- randomarray/static/script.js
+++ randomarray/static/script.js
@@ -1,2 +1,27 @@
-var select_value = document.getElementById('checkbox');
-select_value.addEventListener('onclick', check());
+var duplicate = document.getElementById('min');
+duplicate.addEventListener('click', yes());
+
+window.onload = function yes(){
+    alert('yes')
+};
+
+var main = document.getElementById('main');
+main.addEventListener('click', yes());
+
+function check(){
+    print("yes")
+    var min = document.getElementById('min');
+    var max = document.getElementById('max');
+    var amount = document.getElementById('amount');
+    var generate = document.getElementById('generate');
+    if (duplicate == 'off'){
+        if ((min+max)<amount){
+            generate.disabled = true;
+        }
+        else{
+            generate.disabled = false;
+        }
+
+    }
+}
+
--- randomarray/views.py
+++ randomarray/views.py
@@ -14,19 +14,35 @@ def index(request):
         amount = int(request.POST['amount'])
         type = request.POST['type']
         separate = request.POST['separate']
-        results = generate(min, max, amount, separate, type)
+        duplicate = request.POST['duplicate']
+        print(duplicate)
+        results = generate(min, max, amount, separate, type,duplicate)
     return render(request, 'index.html',{'result' : results})
 
 
-def generate(min, max, amount, separate, type):
+def generate(min, max, amount, separate, type, duplicate):
     result = ""
+    
+
     if separate == "comma":
         sp = ","
     elif separate == "space":
         sp = " "
-    if type == "int":
-        for i in range(amount):
-            result= result + str(random.randint(min, max)) + sp
-
+    
+    
+    if (type == "int") :
+        #중복 허용
+        if duplicate == "on":
+            for i in range(amount):
+                result= result + str(random.randint(min, max)) + sp
+        #중복 허용하지 않음
+        if duplicate == "off":
+            array = []
+            for i in range(max):
+                array.append(str(i))
+            for i in range(amount):
+                result = result + str(array[random.randint(0,len(array))]) + sp
+                array = ''.join(array).split()
+    #마지막 구분 빼기
     result = result[:-1]
     return result
