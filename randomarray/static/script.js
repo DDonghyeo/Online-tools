4d3f711962e73bd16f70fc50f8afd3c79e1c3802
DDonghyeo <gsu9045000@gmail.com>
2022-12-28 03:43:59 +0900

    유효성 검사, 경고 메세지

--- randomarray/static/script.js
+++ randomarray/static/script.js
@@ -1,12 +1,14 @@
-var duplicate = document.getElementById('min')
-var icon = document.getElementById('icon')
-var result = document.getElementById('result')
-var btn = document.getElementById('generate')
-var form = document.getElementsByName('min')
+var min = document.getElementById('min')
+var max = document.getElementById('max')
+var amount = document.getElementById('amount')
+var checkbox = document.getElementById('checkbox')
 var copybtn = document.getElementById("copy")
-console.log(copybtn)
 
 copybtn.addEventListener("click",copy)
+min.addEventListener("keyup",check)
+max.addEventListener("keyup",check)
+amount.addEventListener("keyup",check)
+checkbox.addEventListener("click",check)
 
 function copy() {
     var obj = document.getElementById("result");
@@ -17,24 +19,67 @@ function copy() {
     sel.addRange(range);  //텍스트 정보 선택
     document.execCommand("copy");  //복사
     sel.removeRange(range);  //선택 정보 삭제
-    alert("copied!")
+    alert("copied!");
   }
 
+function isInt(num){
+    return num %1 === 0;
+}
+
+
 function check(){
-    print("yes")
-    var min = document.getElementById('min');
-    var max = document.getElementById('max');
-    var amount = document.getElementById('amount');
+    var min = document.getElementById('min').value
+    var max = document.getElementById('max').value
+    var amount = document.getElementById('amount').value
     var generate = document.getElementById('generate');
-    if (duplicate == 'off'){
-        if ((min+max)<amount){
+    var warning = document.getElementById('warning')
+
+    //중복 허용 X일 경우
+    if (!document.getElementById('checkbox').checked){
+        if (( max - min + 1 )<amount){
             generate.disabled = true;
+            warning.innerHTML = "범위만큼 생성할 수 없습니다.";
+            warning.style.display = "block";
+            return
         }
         else{
             generate.disabled = false;
+            warning.style.display = "none"
         }
-
     }
+
+    //최소값이 최대값보다 클 경우
+    if(min>max){
+        generate.disabled = true;
+        warning.innerHTML = "최소값이 최대값보다 큽니다."
+        warning.style.display = "block"
+        return
+    }else {
+        generate.disabled = false;
+        warning.style.display = "none"
+    }
+
+     //정수가 아닌 값이 입력됐을 경우
+     if (!isInt(min) || !isInt(max) || !isInt(amount)){
+        generate.disabled = true;
+        warning.innerHTML = "정수만 입력 가능합니다."
+        warning.style.display = "block"
+        return
+    }else {
+        generate.disabled = false;
+        warning.style.display = "none"
+    }
+
+    //아무 값을 입력하지 않았을 경우
+    if (min == "" || max == "" || amount == "" || !isInt(min) || !isInt(max) || !isInt(amount)){
+        generate.disabled = true;
+        warning.innerHTML = "값을 입력해야 합니다."
+        warning.style.display = "block"
+    }else {
+        generate.disabled = false;
+        warning.style.display = "none"
+    }
+   
 }
 
 
